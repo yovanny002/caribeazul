e32603a49be0fa26e700053ae6e76ef2c6640bd7
automata103 <automatizacionessirtech@gmail.com>
2025-07-28 19:19:22 -0400

    ****

--- controllers/prestamoInteresController.js
+++ controllers/prestamoInteresController.js
@@ -40,8 +40,28 @@ exports.show = async (req, res) => {
   }
 };
 
-exports.createForm = (req, res) => {
-  res.render('prestamos_interes/create');
+exports.createForm = async (req, res) => {
+  try {
+    // Obtener listado de clientes y rutas desde la base de datos
+    const [clientes, rutas] = await Promise.all([
+      db.query('SELECT id, nombre, apellidos, cedula, profesion FROM clientes ORDER BY nombre', {
+        type: QueryTypes.SELECT
+      }),
+      db.query('SELECT id, nombre, zona FROM rutas ORDER BY nombre', {
+        type: QueryTypes.SELECT
+      })
+    ]);
+
+    res.render('prestamos_interes/create', {
+      clientes,
+      rutas,
+      body: req.body || {} // Para repoblar el formulario en caso de error
+    });
+  } catch (error) {
+    console.error('Error al cargar formulario de préstamo:', error);
+    req.flash('error', 'Error al cargar formulario de préstamo');
+    res.redirect('/prestamos-interes');
+  }
 };
 
 exports.create = async (req, res) => {
