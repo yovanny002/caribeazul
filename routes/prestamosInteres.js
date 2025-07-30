63032f2dd9c8e2cce34a4c720a4f9989b78b2c47
automata103 <automatizacionessirtech@gmail.com>
2025-07-30 13:07:13 -0400

    create de interes pagos

--- routes/prestamosInteres.js
+++ routes/prestamosInteres.js
@@ -5,53 +5,51 @@ const { checkRole } = require('../middlewares/roles');
 
 // Listado de préstamos (GET)
 router.get('/', 
-  checkRole(['administrador', 'supervisor', 'servicio']), 
-  prestamoInteresController.index
+  checkRole(['administrador', 'supervisor', 'servicio']), 
+  prestamoInteresController.index
 );
 
 // Formulario de creación (GET)
 router.get('/create', 
-  checkRole(['administrador', 'supervisor', 'servicio']), 
-  prestamoInteresController.createForm
+  checkRole(['administrador', 'supervisor', 'servicio']), 
+  prestamoInteresController.createForm
 );
 
-// Crear nuevo préstamo (POST) - Versión recomendada (RESTful)
+// Crear nuevo préstamo (POST)
 router.post('/', 
-  checkRole(['administrador', 'supervisor', 'servicio']), 
-  prestamoInteresController.create
+  checkRole(['administrador', 'supervisor', 'servicio']), 
+  prestamoInteresController.create
 );
 
-// Alternativa: Ruta POST /create (mantener solo una de las dos)
-// router.post('/create', 
-//   checkRole(['administrador', 'supervisor', 'servicio']), 
-//   prestamoInteresController.create
-// );
-router.get('/:id/imprimir-contrato',
-  checkRole(['administrador', 'supervisor', 'servicio']),
-  prestamoInteresController.imprimirContrato
-);
 // Detalle de préstamo (GET)
+// === ESTA ES LA RUTA QUE DEBE CAPTURAR EL ID ===
 router.get('/:id', 
-  checkRole(['administrador', 'supervisor', 'servicio']), 
-  prestamoInteresController.show
+  checkRole(['administrador', 'supervisor', 'servicio']), 
+  prestamoInteresController.show
 );
 
 // Formulario de pago (GET)
 router.get('/:id/pago', 
-  checkRole(['administrador', 'supervisor', 'servicio']), 
-  prestamoInteresController.pagoForm
+  checkRole(['administrador', 'supervisor', 'servicio']), 
+  prestamoInteresController.pagoForm
 );
 
 // Registrar pago (POST)
 router.post('/:id/pago', 
-  checkRole(['administrador', 'supervisor', 'servicio']), 
-  prestamoInteresController.registrarPago
+  checkRole(['administrador', 'supervisor', 'servicio']), 
+  prestamoInteresController.registrarPago
 );
 
 // Recibo de pago (GET)
 router.get('/:id/recibo/:pagoId', 
-  checkRole(['administrador', 'supervisor', 'servicio']), 
-  prestamoInteresController.recibo
+  checkRole(['administrador', 'supervisor', 'servicio']), 
+  prestamoInteresController.recibo
+);
+
+// Ruta para imprimir el contrato del préstamo por interés
+router.get('/:id/imprimir-contrato',
+  checkRole(['administrador', 'supervisor', 'servicio']),
+  prestamoInteresController.imprimirContrato
 );
 
 module.exports = router;
