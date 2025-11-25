395395145bddbee979d5dc93955bbb1f8f9e3da7
yovanny002 <yovannyr87@gmail.com>
2025-11-25 14:27:13 -0400

    se modificao la ruta3

--- routes/contratos.js
+++ routes/contratos.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const contratoController = require('../controllers/contratoController');
 
 router.get('/', contratoController.index);
-router.get('financiamiento/create', contratoController.createForm);
-router.post('financiamiento/create', contratoController.create);
+router.get('/create', contratoController.createForm);
+router.post('/create', contratoController.create);
 router.get('/:id', contratoController.show);
 router.get('/:id/download', contratoController.download);
 router.get('/:id/print', contratoController.print);
